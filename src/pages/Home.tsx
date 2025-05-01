1f34ebf8f9c86766216c8b40242a8659325657c9
flairconny <6ditd7t@mail.ru>
2025-05-01 13:38:36 +0700

    Clean unused React imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-
+import { useEffect, useState } from "react";
 import { getAssistants, createChat } from "../api/minibot";
 import ChatWindow from "../components/ChatWindow";
 import { useSearchParams } from "react-router-dom";
